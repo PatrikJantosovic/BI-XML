2531de6e6b44ff60312e5c6ed6a5e19e36641122
Patrik Jantošovič <patrik.jantosovic@gmail.com>
2019-05-04 21:10:25 +0200

    Zmena commandu v noverj verzii saxonu

--- generatepdf.sh
+++ generatepdf.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+java -jar src/saxon.jar output/countries.xml src/ToPDF.xsl > output/countries.fo 
 fop -fo output/countries.fo -pdf output/countries.pdf 2>/dev/null
 echo 'Vygenerovaný pdf súbor v zložke output'
